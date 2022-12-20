0d854850c55fdc9300d3a29cd87d601821a4029b
YOYO-DR <yduran941@misena.edu.co>
2022-12-20 16:39:55 -0500

    20/12/2022

--- login.py
+++ login.py
@@ -2,14 +2,15 @@ from tkinter import *
 import os
 from BD.conexionBD import *
 from tkinter import messagebox
+from principal import VentanaPrincipal
 
-class Login:
+class VentanaLogin:
   def __init__(self):
     #creo la ventana
     self.login = Tk()
     self.login.config(bg='#a6a6a6')
     #asigno tamaño y posicion
-    self.login.geometry("400x500+1800+100")
+    self.login.geometry("400x500+470+85")
     #bloqueo la modificacion de las posiciones
     self.login.resizable(width=False, height=False)
     #titulo de la ventana
@@ -72,23 +73,142 @@ class Login:
       messagebox.showwarning('Inicio se sesión','''No se encontro el usuario, vuelve a intentar. 
 Si es nuevo, presiona en registrar para crear un usuario.''')
       self.ValorUsuario.set('')
+      self.ValorContraseña.set('')
     else:
       valor=verificarUsuario(usuario,contraseña)
       if valor==True:
-        #confirmacion de inicio de sesión
-        
-        messagebox.showinfo('Inicio de sesión','Inicio de sesión correcto.')
-        
+        self.login.destroy()
+        if __name__ == '__main__':
+          VentanaPrincipal()
+
       else:
         messagebox.showwarning('Inicio de sesión','Contraseña incorrecta, vuelve a intentar.')
         self.ValorContraseña.set('')
 
   #registrar usuario
   def FRegistro(self):
-    messagebox.showinfo('Registro','Funcion proxima')
+    self.login.destroy()
+    if __name__ == '__main__':
+          VentanaRegistro()
 
   #cancelar inicio
   def FCancelar(self):
     self.login.destroy()
 
-ejecucion=Login()
+#------------------------------------Ventana registro--------------------------------
+
+class VentanaRegistro:
+  def __init__(self):
+    self.registro = Tk()
+    self.registro.config(bg='#a6a6a6')
+    #asigno tamaño y posicion
+    self.registro.geometry("400x500+470+85")
+    #bloqueo la modificacion de las posiciones
+    self.registro.resizable(width=False, height=False)
+    #titulo de la ventana
+    self.registro.title('Administrador de operaciones')
+    #ruta de la carpeta Img
+    self.carpetaMedia=os.path.join(os.path.join(os.path.dirname(__file__),'media'),'img')
+    self.carpetaLogin=os.path.join(self.carpetaMedia,'login')
+    #logo de la ventana
+    self.registro.iconbitmap(os.path.join(self.carpetaMedia,'tradingIco.ico'))
+#----------------------------------------------------------------#
+    self.bienvenida=Label(self.registro,bg='#a6a6a6', text='Registro',font=('Leelawadee UI Semilight',20)).place(x=160,y=200)
+
+    #Foto de bienvenida
+    self.fotoB=PhotoImage(file=os.path.join(self.carpetaLogin,'signup_100_2.png')).zoom(2)
+    Label(self.registro,image=self.fotoB,bd=0).place(x=105,y=-10)
+
+    #Entry de usuario
+    self.nombre=Label(self.registro,bg='#a6a6a6', text='Usuario:',font=('Leelawadee UI Semilight',15)).place(x=117,y=274)
+    self.ValorUsuario=StringVar()
+    self.usuario=Entry(self.registro,width=18,bg='#8c8c8c',font=('Leelawadee UI Semilight',13),fg='white',textvariable=self.ValorUsuario)
+    self.usuario.place(x=195,y=280)
+
+    #entry de contraseña
+    self.contraseña=Label(self.registro,bg='#a6a6a6', text='Contraseña:',font=('Leelawadee UI Semilight',15)).place(x=85,y=311)
+    self.ValorContraseña=StringVar()
+    self.contraseña=Entry(self.registro,width=18,bg='#8c8c8c',font=('Leelawadee UI Semilight',13),fg='white',textvariable=self.ValorContraseña,show='*').place(x=195,y=316)
+   
+   #entry de confirmacion de contraseña
+    self.confiContra=Label(self.registro,bg='#a6a6a6', text='Confirmar contraseña:',font=('Leelawadee UI Semilight',15)).place(x=1,y=346)
+    self.ValorConfiContra=StringVar()
+    self.confiContra=Entry(self.registro,width=18,bg='#8c8c8c',font=('Leelawadee UI Semilight',13),fg='white',textvariable=self.ValorConfiContra,show='*').place(x=195,y=351)
+
+    #boton registrar
+    self.botonAceptar=Button(self.registro,bg='#038554',text='Registrar',command=self.FRegistro)
+    self.botonAceptar.place(x=125,y=390)
+    self.botonAceptar.config(width=22)
+
+    #boton volver login
+    self.botonVLogin=Button(self.registro,bg='#8c8c8c',text='Volver al inicio',command=self.volverLogin)
+    self.botonVLogin.place(x=125,y=420)
+    self.botonVLogin.config(width=22)
+
+    #label separador
+    self.separador=Canvas(self.registro,width=164,height=3,bd=-2,bg='grey40')
+    self.separador.place(x=125,y=452)
+
+    #boton cancelar
+    self.botonCancelar=Button(self.registro,bg='#ac3a3a',text='Cancelar',command=self.FCancelar)
+    self.botonCancelar.place(x=125,y=460)
+    self.botonCancelar.config(width=22)
+
+
+    self.registro.mainloop()
+
+  def volverLogin(self):
+    self.registro.destroy()
+    if __name__ == '__main__':
+        VentanaLogin()
+
+  def FCancelar(self):
+      self.registro.destroy()
+      
+
+
+  def FRegistro(self):
+    usuario=self.ValorUsuario.get()
+    usuario=usuario.strip()
+    verificar=buscarUsuario(usuario)
+
+    contraseña=self.ValorContraseña.get()
+    contraseña=contraseña.strip()
+
+    confi=self.ValorConfiContra.get()
+    confi=confi.strip()
+
+    if verificar==True:
+      messagebox.showwarning('Usuario','El nombre de usuario ya existe, elige otro.')
+      self.ValorUsuario.set('')
+      self.ValorContraseña.set('')
+      self.ValorConfiContra.set('')
+    elif usuario=='':
+      messagebox.showwarning('Usuario','Usuario no puede estar vacio.')
+      self.ValorContraseña.set('')
+      self.ValorConfiContra.set('')
+    elif contraseña=='':
+      messagebox.showwarning('Contraseña','La contraseña no puede estar vacio.')
+    elif confi=='':
+      messagebox.showwarning('Confirmacion','La confirmacion, no puede estar vacio.')
+      #Terminar ventana de registro
+    elif len(contraseña)<8 or len(contraseña)>12:
+      messagebox.showerror('Tamaño contraseña','La contraseña debe ser de minimo 8 y maximo de 12 caracteres.')
+      self.ValorContraseña.set('')
+      self.ValorConfiContra.set('')
+    elif contraseña!=confi:
+      messagebox.showerror('Confirmacion contraseña','La confirmacion no es igual a la contraseña.')
+      self.ValorContraseña.set('')
+      self.ValorConfiContra.set('')
+    else:
+      try:
+        registrar(usuario,contraseña)
+        messagebox.showinfo('Registro','Usuario registrado')
+        self.registro.destroy()
+        if __name__ == '__main__':
+          VentanaLogin()
+      except:
+        messagebox.showerror('Error','Hubo un problema al registrar al usuario, vuelve a intentar.')
+
+VentanaLogin()
+
--- principal.py
+++ principal.py
@@ -1,7 +1,106 @@
 from tkinter import *
-class Principal:
+import os
+
+class VentanaPrincipal:
   def __init__(self):
     self.root=Tk()
+    self.root.geometry('1050x600+1500+50')
+    self.root.title('AdminTrading')
     
+    #---------------------------------carpeta media---------------------------------#
+    self.carpetaMedia=os.path.join(os.path.dirname(__file__),'media')
+
+    #------------------------------carpeta de imagenes------------------------------#
+    self.carpetaImg=os.path.join(self.carpetaMedia,'img')
+    
+    #------------------------icono ventana y barra de inicio------------------------#
+    self.iconoVentana=PhotoImage(file=os.path.join(self.carpetaImg,'icono.png'))
+    self.root.iconphoto(False, self.iconoVentana,self.iconoVentana)
+
+    #-----------------------elementos del apartado de la barra-----------------------#
+    #apartado de barra de logo y titulo
+    self.operacionesC=Canvas(self.root,width=1050,height=40,bg='grey90')
+    self.operacionesC.place(x=0,y=0)
+
+    #logo
+    self.logoB=PhotoImage(file=os.path.join(self.carpetaImg,'logoAdmin.png')).subsample(15)
+    Label(self.root,image=self.logoB,bg='grey90').place(x=0,y=2)
+
+    #label del titulo en barra
+    self.tituloB=Label(self.root,text='Administrador',bg='grey90',font=('Leelawadee UI Semilight',14,'bold'))
+    self.tituloB.place(x=35,y=5)
+
+
+
+    #----------------------elementos del apartado de operaciones----------------------#
+    #apartado de operaciones
+    self.operacionesC=Canvas(self.root,width=500,height=195,bg='grey90')
+    self.operacionesC.place(x=10,y=50)
+
+    #Label del titulo de operaciones
+    self.tituloIngresar=Label(self.root,text='Ingresar operacion',bg='grey90',font=('Leelawadee UI Semilight',14))
+    self.tituloIngresar.place(x=20,y=55)
+
+    #----------------------elementos del apartado de calculadora----------------------#
+    #apartado de calculadora
+    self.operacionesC=Canvas(self.root,width=500,height=195,bg='grey90')
+    self.operacionesC.place(x=10,y=255)
+
+    #Label del titulo de calculadora
+    self.tituloIngresar=Label(self.root,text='Calculadora',bg='grey90',font=('Leelawadee UI Semilight',14))
+    self.tituloIngresar.place(x=20,y=260)
+
+    #--------------------elementos del apartado de ganancias/perdidas--------------------#
+    #apartado de ganancias/perdidas
+    self.operacionesC=Canvas(self.root,width=500,height=130,bg='grey90')
+    self.operacionesC.place(x=10,y=460)
+
+    #Label del titulo de ganancias/perdidas
+    self.tituloIngresar=Label(self.root,text='Ganancias/perdidas',bg='grey90',font=('Leelawadee UI Semilight',14))
+    self.tituloIngresar.place(x=20,y=465)
+
+    #canvas total
+    self.CTotal=Canvas(self.root,width=150,height=80,bg='grey90',bd=2)
+    self.CTotal.place(x=16,y=500)
+
+    #canvas mes
+    self.CTotal=Canvas(self.root,width=150,height=80,bg='grey90',bd=2)
+    self.CTotal.place(x=183,y=500)
+
+    #canvas semana
+    self.CTotal=Canvas(self.root,width=150,height=80,bg='grey90',bd=2)
+    self.CTotal.place(x=350,y=500)
+
+    #--------------------elementos del apartado de buscaroperaciones--------------------#
+    #apartado de ganancias/perdidas
+    self.operacionesC=Canvas(self.root,width=510,height=350,bg='grey90')
+    self.operacionesC.place(x=525,y=50)
+
+    #Label del titulo de buscar operaciones
+    self.tituloIngresar=Label(self.root,text='Buscar operaciones',bg='grey90',font=('Leelawadee UI Semilight',14))
+    self.tituloIngresar.place(x=530,y=55)
+
+    #--------------------elementos del apartado de actualizar/borrar/logo--------------------#
+    
+    #apartado de actualizar/borrar/logo
+    self.operacionesC=Canvas(self.root,width=510,height=180,bg='grey90')
+    self.operacionesC.place(x=525,y=410)
+
+    #logo
+    self.logoVelas=PhotoImage(file=os.path.join(self.carpetaImg,'velas_japonesas.png')).subsample(3)
+    Label(self.root,image=self.logoVelas,bg='grey90').place(x=660,y=430)
+
+    """ #label acturalizar
+    self.actualizarL=Label(self.root,text='¿Desea actualizar la operacion n° ?',bg='grey90',font=('Leelawadee UI Semilight',14))
+    self.actualizarL.place(x=535,y=415) """
+    
+    #label borrar
+    self.actualizarL=Label(self.root,text='¿Desea borrar la operacion n° ?',bg='grey90',font=('Leelawadee UI Semilight',14))
+    self.actualizarL.place(x=535,y=415)
+
+
+
     self.root.mainloop()
-eje=Principal()
+
+
+VentanaPrincipal()
